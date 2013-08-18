41c44f3da9b7980eca998498a001577c521d8acd
Kendrick Kay <kendrick@post.harvard.edu>
2013-08-18 16:51:50 -0500

    in the cases of empty <X>, we now return 1 instead of [].

--- stats/projectionmatrix.m
+++ stats/projectionmatrix.m
@@ -13,9 +13,21 @@ function f = projectionmatrix(X)
 % dimensions samples x samples.  to accomplish this,
 % we rely heavily on olsmatrix.m.
 %
+% if <X> has no parameters, the output of this function is 1.
+%
+% history:
+% - 2013/08/18 - in the cases of empty <X>, we now return 1 instead of [].
+%
 % example:
 % x = sort(randn(100,1));
 % x2 = projectionmatrix(constructpolynomialmatrix(100,0:1))*x;
 % figure; hold on; plot(x,'r-'); plot(x2,'g-');
 
+% handle corner case
+if isempty(X)
+  f = 1;
+  return;
+end
+
+% do it
 f = eye(size(X,1)) - X*olsmatrix(X);
