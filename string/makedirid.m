e90a1fa7ada1d57b50d6db94b4853188db49ce90
Kendrick Kay <kendrick@post.harvard.edu>
2013-08-18 16:59:05 -0500

    now occurrences of '-' are replaced with '_'

--- string/makedirid.m
+++ string/makedirid.m
@@ -6,9 +6,14 @@ function f = makedirid(x,num)
 % <num> (optional) is the non-negative number of elements desired.  default: 2.
 %
 % return a string with the last <num> elements of the path joined with '_'.
+% also, any occurrences of '-' are replaced with '_'.
+%
+% history:
+% - 2013/08/18 - now, '-' is replaced with '_' (so that the result
+%   can be a valid function name).
 %
 % example:
-% isequal(makedirid('/home/knk/dir1/file'),'dir1_file')
+% isequal(makedirid('/home/knk/dir1/file-test'),'dir1_file_test')
 
 % input
 if ~exist('num','var') || isempty(num)
@@ -22,3 +27,6 @@ for p=1:num
   x = stripfile(x);
 end
 f = f(2:end);
+
+% replace - with _
+f = strrep(f,'-','_');
