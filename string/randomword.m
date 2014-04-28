2db79f455ea9eb76df7d0202dfbc47def8be7b4d
Kendrick Kay <kendrick@post.harvard.edu>
2014-04-27 20:15:33 -0500

    add additional input cases

--- string/randomword.m
+++ string/randomword.m
@@ -6,6 +6,8 @@ function f = randomword(n,mode)
 % <mode> (optional) is
 %   0 means choose from capital letters
 %   1 means choose from capital and lowercase letters and digits
+%   2 means choose from capital and lowercase letters
+%   3 means choose from lowercase letters
 %   default: 0.
 %
 % return a random word consisting of <n> characters.
@@ -24,6 +26,10 @@ case 0
   rng = [64+(1:26)];
 case 1
   rng = [64+(1:26) 96+(1:26) 47+(1:10)];
+case 2
+  rng = [64+(1:26) 96+(1:26)];
+case 3
+  rng = [96+(1:26)];
 end
 %f = char(rng(randint(1,n,[1 length(rng)])));
 f = char(rng(ceil(rand(1,n)*length(rng))));
