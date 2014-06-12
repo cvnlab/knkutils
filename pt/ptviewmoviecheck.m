1c30448dfc8f481f5759e2b142adb463bdec6075
Kendrick Kay <kendrick@post.harvard.edu>
2014-06-12 13:48:17 -0500

    revamp to function behavior. now return useful outputs.

--- pt/ptviewmoviecheck.m
+++ pt/ptviewmoviecheck.m
@@ -1,21 +1,28 @@
-function ptviewmoviecheck(timeframes,timekeys,deltatime,badkey)
+function [keytimes,badtimes] = ptviewmoviecheck(timeframes,timekeys,deltatime,badkey,deltatimeBAD)
 
-% function ptviewmoviecheck(timeframes,timekeys,deltatime,badkey)
+% function [keytimes,badtimes] = ptviewmoviecheck(timeframes,timekeys,deltatime,badkey,deltatimeBAD)
 % 
 % <timeframes>,<timekeys> are outputs from ptviewmovie.m
 % <deltatime> (optional) is the time differential in seconds that must
 %   be exceeded for a key that is detected twice in a row to be
-%   counted as an actual keypress for display purposes.  default: 1.
-% <badkey> (optional) is a character.  ignore entries in <timekeys>
+%   counted as an actual keypress.  default: 1.
+% <badkey> (optional) is a character.  separately process entries in <timekeys>
 %   for which the first character matches <badkey>.  if [] do nothing special.
+%   default: [].
+% <deltatimeBAD> (optional) is like <deltatime> but for <badkey>.  default: 0.25.
 %
 % draw some figures visualizing the contents of <timeframes> and <timekeys>.
+% return a vector with keypress times in <keytimes> and a vector with
+% badkey times in <badtimes>.
+%
+% history:
+% - 2014/06/12 - revamp to function behavior. now return useful outputs.
 %
 % example:
 % pton;
 % [timeframes,timekeys] = ptviewmovie(uint8(255*rand(100,100,3,100)),[],[],2,[],[],[],[],[],0);
 % ptoff;
-% ptviewmoviecheck(timeframes,timekeys);
+% [keytimes,badtimes] = ptviewmoviecheck(timeframes,timekeys);
 
 % input
 if ~exist('deltatime','var') || isempty(deltatime)
@@ -23,6 +30,9 @@ if ~exist('deltatime','var') || isempty(deltatime)
 end
 if ~exist('badkey','var') || isempty(badkey)
   badkey = [];
+end
+if ~exist('deltatimeBAD','var') || isempty(deltatimeBAD)
+  deltatimeBAD = 0.25;
 end
 
 % look at difference in frame times
@@ -32,8 +42,8 @@ title('inspection of timeframes');
 
 % look at timekeys
 drawnow; figure; setfigurepos([50 50 800 300]); hold on;
-oldkey = ''; oldkeytime = -Inf; cnt = 0;
-  % first we have to expand the multiple key press cases
+
+% first we have to expand the multiple-keypresses cases
 timekeysB = {};
 for p=1:size(timekeys,1)
   if iscell(timekeys{p,2})
@@ -45,19 +55,47 @@ for p=1:size(timekeys,1)
     timekeysB(end+1,:) = timekeys(p,:);
   end
 end
-  % then we can process
+
+% now process non-badkeys
+oldkey = ''; oldkeytime = -Inf; cnt = 0;
+keytimes = [];
+xvals = [];
 for p=1:size(timekeysB,1)
   if ~isequal(timekeysB{p,2},'absolutetimefor0') && ...
      (isempty(badkey) || ~isequal(timekeysB{p,2}(1),badkey)) && ...
-     (~isequal(timekeysB{p,2},oldkey) || timekeysB{p,1}-oldkeytime > deltatime)  % if we have found a new key
+     (timekeysB{p,1}-oldkeytime > deltatime)     %%%REMOVED ~isequal(timekeysB{p,2},oldkey) ||  [this means all the same]
+    keytimes = [keytimes timekeysB{p,1}];  % record
     straightline(timekeysB{p,1},'v',[getcolorchar(cnt) '-']);
-    text(timekeysB{p,1},1.1 + 0.5*rand,timekeysB{p,2},'HorizontalAlignment','center','Color',getcolorchar(cnt));
+    text(timekeysB{p,1},1.1 + 0.5*rand,timekeysB{p,2}(1),'HorizontalAlignment','center','Color',getcolorchar(cnt));
     cnt = cnt + 1;
     oldkey = timekeysB{p,2};
     oldkeytime = timekeysB{p,1};
+    xvals = [xvals timekeysB{p,1}];
   end
 end
-xlabel('time');
+
+% short in the y-direction so the badkeys are short vertical lines
 ax = axis;
-axis([ax(1:2) 0 1.6]);
+axis([ax(1:2) 0 0.5]);
+
+% now process badkeys
+oldkey = ''; oldkeytime = -Inf; cnt = 0;
+badtimes = [];
+for p=1:size(timekeysB,1)
+  if ~isequal(timekeysB{p,2},'absolutetimefor0') && ...
+     (~isempty(badkey) && isequal(timekeysB{p,2}(1),badkey)) && ...
+     (timekeysB{p,1}-oldkeytime > deltatimeBAD)  % if we have found a new key
+    badtimes = [badtimes timekeysB{p,1}];  % record
+    set(straightline(timekeysB{p,1},'v','k-'),'Color',[.7 .7 .7]);
+%%    text(timekeysB{p,1},1.1 + 0.5*rand,timekeysB{p,2}(1),'HorizontalAlignment','center','Color',getcolorchar(cnt));
+    cnt = cnt + 1;
+    oldkey = timekeysB{p,2};
+    oldkeytime = timekeysB{p,1};
+    xvals = [xvals timekeysB{p,1}];
+  end
+end
+
+% finish up figure
+xlabel('time (seconds)');
+axis([floor(min(xvals)-1) ceil(max(xvals)+1) 0 1.6]);
 title(sprintf('inspection of timekeys (repeated keys within %.1f s are not shown)',deltatime));
