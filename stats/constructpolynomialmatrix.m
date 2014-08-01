d0a62d9d9486cd56321e4a52be016339b5af61dc
Kendrick Kay <kendrick@post.harvard.edu>
2014-07-31 20:40:58 -0500

    now polynomials are orthogonalized and unit length!

--- stats/constructpolynomialmatrix.m
+++ stats/constructpolynomialmatrix.m
@@ -6,16 +6,31 @@ function f = constructpolynomialmatrix(n,degrees)
 % <degrees> is a vector of polynomial degrees
 %
 % return a matrix of dimensions <n> x length(<degrees>)
-% with polynomials in the columns (e.g. x, x^2, x^3, etc.).
-% these are evaluated over the range [-1,1].
+% with polynomials in the columns.  each column is orthogonalized
+% with respect to all of the earlier columns and then made 
+% unit-length.  please see the code for the exact details.
 % beware of numerical precision issues for high degrees...
 %
+% history:
+% - 2014/07/31 - now, we orthogonalize and make unit length.
+%                this changes previous behavior!
+%
 % example:
-% figure; imagesc(constructpolynomialmatrix(100,0:3));
+% X = constructpolynomialmatrix(100,0:3);
+% figure; subplot(1,2,1); imagesc(X); subplot(1,2,2); imagesc(X'*X);
 
 % do it
 f = [];
 temp = linspace(-1,1,n)';
 for p=1:length(degrees)
-  f = cat(2,f,temp .^ degrees(p));
+
+  % construct polynomial
+  polyvector = temp .^ degrees(p);
+  
+  % orthogonalize with respect to earlier polynomials and make unit length
+  polyvector = unitlength(projectionmatrix(f)*polyvector);
+
+  % record
+  f = cat(2,f,polyvector);
+  
 end
