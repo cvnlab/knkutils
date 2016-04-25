83e6a8638068d77608ea9d14004afa1e43a3e5ec
Kendrick Kay <kendrick@post.harvard.edu>
2016-04-25 10:01:53 -0500

    add inputs, outputs

--- mri/computetemporalsnr.m
+++ mri/computetemporalsnr.m
@@ -1,37 +1,53 @@
-function f = computetemporalsnr(vols)
+function [tsnr,mn,mad] = computetemporalsnr(vols,dim)
 
-% function f = computetemporalsnr(vols)
+% function [tsnr,mn,mad] = computetemporalsnr(vols,dim)
 %
-% <vols> is X x Y x Z x T with a 3D volume over time.
+% <vols> is X x Y x ... x T with time-series along the last dimension (must be 2 or higher).
+% <dim> (optional) is the dimension with time-series data. Default: ndims(vols).
 %
-% return a matrix of size X x Y x Z with the temporal SNR.
+% return <tsnr> as a matrix of size X x Y x ... with the temporal SNR.
+% also, return <mn> with the mean (of the original time-series).
+% also, return <mad> with the median absolute difference (see below).
+%
 % the temporal SNR is defined as follows:
 % 1. first regress out a constant and a line from the time-series
 %    of each voxel.
 % 2. then compute the absolute value of the difference between each
 %    pair of successive time points (if there are N time points,
 %    there will be N-1 differences).
-% 3. compute the median absolute difference.
+% 3. compute the median absolute difference (mad).
 % 4. divide by the mean of the original time-series and multiply by 100.
 % 5. if any voxel had a negative mean, just return the temporal SNR as NaN.
 %
 % the purpose of the differencing of successive time points is to be relatively
 % insensitive to actual activations (which tend to be slow), if they exist.
 %
-% if <vols> is [], we return [].
+% if <vols> is [], we return [] for all outputs.
 %
 % example:
 % vols = getsamplebrain(4);
-% figure; imagesc(makeimagestack(5-computetemporalsnr(vols))); caxis([0 5]); colormap(jet); colorbar;
+% [tsnr,mn,mad] = computetemporalsnr(vols);
+% figure; imagesc(makeimagestack(mn));   caxis([0 2500]); axis image; colormap(gray); colorbar;
+% figure; imagesc(makeimagestack(mad));  caxis([0 100]);  axis image; colormap(gray); colorbar;
+% figure; imagesc(makeimagestack(tsnr)); caxis([0 5]);    axis image; colormap(jet);  colorbar;
 
 % internal constants
 maxtsnrpolydeg = 1;
 
+% calc
+if ~exist('dim','var') || isempty(dim)
+  dim = ndims(vols);
+end
+
 % do it
 if isempty(vols)
-  f = [];
+  tsnr = [];
+  mn = [];
+  mad = [];
 else
-  f = negreplace(median(abs(diff( ...
-        reshape((projectionmatrix(constructpolynomialmatrix(size(vols,4),0:min(size(vols,4)-1,maxtsnrpolydeg)))*squish(vols,3)')',size(vols)), ...
-        1,4)),4)./mean(vols,4) * 100,NaN);
+  mn = mean(vols,dim);
+  mad = median(abs(diff( ...
+           reshape((projectionmatrix(constructpolynomialmatrix(size(vols,dim), ...
+             0:min(size(vols,dim)-1,maxtsnrpolydeg)))*squish(vols,dim-1)')',size(vols)),1,dim)),dim);
+  tsnr = negreplace(mad./mn * 100,NaN);
 end
