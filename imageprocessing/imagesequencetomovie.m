4e4dcc728e21765dd8d6fe6a125505c7589fcecb
Kendrick Kay <kendrick@post.harvard.edu>
2016-09-03 12:34:15 -0500

    make more flexible and less memory usage

--- imageprocessing/imagesequencetomovie.m
+++ imageprocessing/imagesequencetomovie.m
@@ -1,13 +1,15 @@
-function imagesequencetomovie(dir0,file0,framerate)
+function imagesequencetomovie(images0,mov0,framerate)
 
-% function imagesequencetomovie(dir0,file0,framerate)
+% function imagesequencetomovie(images0,mov0,framerate)
 %
-% <dir0> is a directory containing image files.
+% <images0> is a path to a directory that contains image files
 %   can also be a 3D or 4D uint8 matrix of images.
-% <file0> is output movie file to write
-% <framerate> is the frames per second for the output movie
+% <mov0> (optional) is output movie file to write.
+%   If <images0> is a directory, we default to [images0 '.mov'].
+% <framerate> (optional) is the frames per second for the output movie.
+%   Default: 10.
 %
-% Use QTWriter to make a QuickTime movie from the images in <dir0>.
+% Use QTWriter to make a QuickTime movie from the images in <images0>.
 %
 % example:
 % mkdirquiet('temp');
@@ -16,22 +18,52 @@ function imagesequencetomovie(dir0,file0,framerate)
 % end
 % imagesequencetomovie('temp','temp.mov',30);
 
-% init the movie
-mov = QTWriter(file0);
-mov.FrameRate = framerate;
-
-% if a directory, load in the images
-if ischar(dir0)
-  dir0 = imreadmulti([dir0 '/*']);
+% inputs
+if ~exist('framerate','var') || isempty(framerate)
+  framerate = 10;
 end
 
+% if a directory, load in the images
+if ischar(images0)
+
+  % get the dir
+  images0 = matchfiles(images0);
+
+  % make sure just one
+  assert(length(images0)==1);
+  images0 = images0{1};
+
+  % strip /
+  if isequal(images0(end),filesep)
+    images0 = images0(1:end-1);
+  end
+  
+  % save the directory name
+  dirname0 = images0;
+
+  % read all the images
+  images0 = imreadmulti(fullfile(images0,'*'));
+
+end
+
+% inputs
+if ~exist('mov0','var') || isempty(mov0)
+  mov0 = [dirname0 '.mov'];
+end
+
+% init the movie
+mov = QTWriter(mov0);
+mov.FrameRate = framerate;
+
 % if 3D grayscale images, reshape them..
-if size(dir0,4)==1
-  dir0 = permute(dir0,[1 2 4 3]);
+if size(images0,4)==1
+  images0 = permute(images0,[1 2 4 3]);
 end
 
 % write the movie
-writeMovie(mov,dir0);
+for p=1:size(images0,4)
+  writeMovie(mov,images0(:,:,:,p));
+end
 
 % finish up
 close(mov);
