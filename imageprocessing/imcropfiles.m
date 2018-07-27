71ae204ee5c49986a140a51612239bfc0849a2e8
Kendrick Kay <kendrick@post.harvard.edu>
2018-07-27 13:19:52 -0500

    add new input

--- imageprocessing/imcropfiles.m
+++ imageprocessing/imcropfiles.m
@@ -1,8 +1,11 @@
-function imcropfiles(files)
+function thecrop = imcropfiles(files,thecrop)
 
-% function imcropfiles(files)
+% function thecrop = imcropfiles(files,thecrop)
 %
 % <files> matches one or more image files
+% <thecrop> (optional) is a previous output of this function.
+%   This is useful if you want to re-use a crop.  If supplied,
+%   we do not need to have any user interaction.
 %
 % this function is for quickly applying a manually
 % defined crop to a bunch of images. it is assumed
@@ -13,6 +16,8 @@ function imcropfiles(files)
 % user presses a key in the command window, we go ahead and apply
 % that crop to all of the images and save the results (overwriting
 % the original files).
+%
+% we return the crop in case you want to reuse it.
 
 % figure out the file paths
 files = matchfiles(files);
@@ -24,7 +29,7 @@ for p=1:length(files)
   im = imread(files{p});
 
   % if the first one, show it and let the user set the axis range
-  if p==1
+  if ~(exist('thecrop','var') && ~isempty(thecrop)) && p==1
     figure;
     image(im);
 %    axis image tight;
@@ -33,11 +38,16 @@ for p=1:length(files)
     rmin = round(pos(2));
     cmax = round(pos(1)+pos(3));
     rmax = round(pos(2)+pos(4));
+    rmin = max(1,rmin);
+    rmax = min(size(im,1),rmax);
+    cmin = max(1,cmin);
+    cmax = min(size(im,2),cmax);
+    thecrop = [rmin rmax cmin cmax];
     close;
   end
   
   % crop image
-  im = im(max(1,rmin):min(size(im,1),rmax),max(1,cmin):min(size(im,2),cmax),:);
+  im = im(thecrop(1):thecrop(2),thecrop(3):thecrop(4),:);
   
   % write out the image
   imwrite(im,files{p});
