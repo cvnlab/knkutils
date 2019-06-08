afc40effc47fc0c45cd7497147bd29a3514b2d1a
Kendrick Kay <kendrick@post.harvard.edu>
2019-06-08 10:57:04 -0500

    add resultsrt output. also report buttons pressed to command window.

--- pt/calcfixationdotchangeperformance.m
+++ pt/calcfixationdotchangeperformance.m
@@ -1,6 +1,6 @@
-function results = calcfixationdotchangeperformance(files)
+function [results,resultsrt] = calcfixationdotchangeperformance(files)
 
-% function results = calcfixationdotchangeperformance(files)
+% function [results,resultsrt] = calcfixationdotchangeperformance(files)
 %
 % <files> matches one or more .mat files that are saved by Kendrick's experiment.
 %   These .mat files should reflect the 'press-button-when-fixation-dot-color-changes'
@@ -8,8 +8,9 @@ function results = calcfixationdotchangeperformance(files)
 %
 % We analyze the button presses in <files> to see how well the subject did.
 % 
-% We report results to the command window.
-% We also return the results in <results> as 4 x N.
+% We report some information to the command window.
+%
+% We also return some results in <results> as 4 x N.
 %   Each column corresponds to a single file that we processed.
 %   The values in each column are [A B C D]
 %     where A is the number of total color changes
@@ -17,18 +18,27 @@ function results = calcfixationdotchangeperformance(files)
 %           C is the number of false alarms (extra button presses)
 %           D is the overall score calculated as (B-C)/A*100
 %
+% We also return reaction times in <resultsrt> as a cell vector of 1 x N. 
+%   Each element is a vector of reaction times in milliseconds.
+%   We record only hits (not false alarms).
+%
 % Note that we assume some internal constants (see code).
+
+% history
+% - 2019/06/08 - add resultsrt output. also report buttons pressed to command window.
 
 % internal constants
 deltatime = 1;    % holding the key down for less than this time will be counted as one button press
 respondtime = 1;  % the subject has this much time to press a button
 validkeys = {'1!' '2@' '3#' '4$' '5%' 'r' 'y' 'g' 'b' 't' 'absolutetimefor0' 'trigger'};
+userkeys = {'1' '2' '3' '4' 'r' 'y' 'g' 'b'};
 
-% get the files (report to the command window)
-files = matchfiles(files)
+% get the files
+files = matchfiles(files);
 
 % loop over files
-results = [];
+results = zeros(4,length(files));
+resultsrt = cell(1,length(files));
 for zz=1:length(files)
 
   % report that we are doing this file
@@ -53,6 +63,7 @@ for zz=1:length(files)
   % figure out when the user pressed a button [buttontimes]
   oldkey = ''; oldkeytime = -Inf;
   buttontimes = [];
+  buttonpressed = {};
   for p=1:size(timekeysB,1)
   
     % warn if weird key found
@@ -70,6 +81,7 @@ for zz=1:length(files)
     % if not bogus, then record the button time
     if ~bad
       buttontimes = [buttontimes timekeysB{p,1}];
+      buttonpressed = [buttonpressed {timekeysB{p,2}(1)}];
       oldkey = timekeysB{p,2};
       oldkeytime = timekeysB{p,1};
     end
@@ -87,6 +99,7 @@ for zz=1:length(files)
     okok = (buttontimes > changetimes(q)) & (buttontimes <= changetimes(q) + respondtime);
     if any(okok)
       numhits = numhits + 1;
+      resultsrt{zz} = [resultsrt{zz} (buttontimes(firstel(find(okok))) - changetimes(q)) * 1000];
       buttontimes(firstel(find(okok))) = [];  % remove!
     end
   end
@@ -95,10 +108,19 @@ for zz=1:length(files)
   numfalse = length(buttontimes);
   finalscore = (numhits-numfalse)/numtot*100;
 
+  % report buttons pressed (just for sanity checking)
+  fprintf('Number of pressed buttons:\n');
+  for pp=1:length(userkeys)
+    fprintf('%s: %d\n',userkeys{pp},sum(ismember(buttonpressed,userkeys{pp})));
+  end
+
   % report score
   fprintf('==============================================================\n');
-  fprintf('Out of %d events, you had %d hits and %d false alarms.\nYour score is %d/%d = %d%%.\n', ...
-          numtot,numhits,numfalse,numhits-numfalse,numtot,round(finalscore));
+  fprintf(['Out of %d events, you had %d hits and %d false alarms.\n' ...
+           'Your score is %d/%d = %d%%.\n' ...
+           'Your median RT is %d ms.\n'], ...
+          numtot,numhits,numfalse,numhits-numfalse,numtot,round(finalscore), ...
+          round(median(resultsrt{zz})));
   fprintf('==============================================================\n');
   
   % record results
