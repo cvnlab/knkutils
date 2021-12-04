14dde3c5795bca1bb00b42879d221eda130897ac
Kendrick Kay <kendrick@post.harvard.edu>
2021-12-04 12:24:37 -0600

    add optional input

--- math/posrect.m
+++ math/posrect.m
@@ -1,12 +1,21 @@
-function m = posrect(m)
+function m = posrect(m,val)
 
-% function m = posrect(m)
+% function m = posrect(m,val)
 %
 % <m> is a matrix
+% <val> (optional) is the cut-off value. Default: 0.
 %
 % positively-rectify <m>.
+% basically do: m(m<val) = val.
 %
 % example:
 % isequal(posrect([2 3 -4]),[2 3 0])
+% isequal(posrect([2 3 -4],4),[4 4 4])
 
-m(m<0) = 0;
+% inputs
+if ~exist('val','var') || isempty(val)
+  val = 0;
+end
+
+% do it
+m(m<val) = val;
