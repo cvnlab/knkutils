2788de573d7d7855b4f8fe269a86e51e9d75903e
Kendrick Kay <kendrick@post.harvard.edu>
2023-12-12 16:48:22 -0600

    tweak

--- programming/evalme.m
+++ programming/evalme.m
@@ -4,6 +4,7 @@ function evalme
 %
 % accept multi-line input from stdin (input is terminated by a single '.'
 % on a new line).  then evaluate the input in the 'caller' workspace.
+% we do a "rehash path" right before we evaluate, just for safety.
 %
 % this function is useful for copy-and-pasting a large chunk of code
 % to be evaluated in the MATLAB workspace.
@@ -14,4 +15,5 @@ function evalme
 % a+1
 % .
 
+rehash path;
 evalin('caller',inputmulti);
