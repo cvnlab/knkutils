47ee6079fcecb63e261bec3c5d90878a8bfd8b2a
Kendrick Kay <kendrick@post.harvard.edu>
2026-05-30 21:12:23 -0500

    overhaul

--- pt/ptviewmoviecheck2.m
+++ pt/ptviewmoviecheck2.m
@@ -16,10 +16,15 @@ function results = ptviewmoviecheck2(timeframes,timekeys,triggerkeys,deltatime,w
 %
 % Draw some figures visualizing the contents of <timeframes> and <timekeys>.
 % In the <timekeys> figure, the <mristarttime> is a black line, <buttontimes> are
-% colored lines (with text indicating <buttonpressed>), and <triggertimes>
-% are light gray lines.
+% colored lines (with text indicating <buttonpressed>), <triggertimes>
+% are light gray lines, and <donetime> is a black line.
 %
 % Return <results> as a struct with:
+%   <droppedcnt> is the number of dropped frames
+%   <mdtf> is the mean frame-to-frame difference
+%   <totaldur> is the total empirical duration of the experiment (partial runs will have low <totaldur>)
+%   <donetime> is the time of the end of the experiment
+%   <timeframes> is like the original, but with linear interpolation to fix dropped fames
 %   <matlabnowtime> is the absolute time for the first frame
 %   <mristarttime> is time in seconds (relative to the time of the first frame)
 %     that the experiment was started
@@ -27,7 +32,15 @@ function results = ptviewmoviecheck2(timeframes,timekeys,triggerkeys,deltatime,w
 %   <buttonpressed> is a cell vector of characters corresponding to <buttontimes>
 %   <triggertimes> is a vector of times in seconds indicating trigger events
 %   <userkeys> is a cell vector of possible user keys (buttons)
-%   <userkeycounts> is a vector of number of times that the keys were pressed.
+%   <userkeycounts> is a vector of number of times that the keys were pressed
+%   <mdf> is the median trigger diff
+%   <mdferrs> is a vector of weird trigger diffs expressed in multiples of the <mdf>
+%   <numcrazymdferrs> is number of <mdferrs> that are deemed crazy/weird
+%   <numwarnings> is the number of warnings detected in this function.
+%     a good sign is if this number is 0.
+%
+% Note that if the total number of triggers is less than 5, we give up and
+% return <mdf> as NaN, <mdferrs> as [], and <numcrazymdferrs> as NaN.
 %
 % Compared to ptviewmoviecheck.m, this function (ptviewmoviecheck2.m) is much
 % more robust and better at corner cases. It is recommended to use this one for
@@ -61,16 +74,14 @@ choicebuttons = {'1' '2' '3' '4'};  % the official buttons we expect subjects to
 
 % init
 clear results;
+results.numwarnings = 0;
 
-%% Start the figures
+%% Make timeframes figure
 
 % look at difference in frame times
-drawnow; figureprep([],~wanthide); plot(diff(timeframes),'r-');
+drawnow; figureprep([50 500 1200 300],~wanthide); plot(diff(timeframes),'r-');
 xlabel('frame difference'); ylabel('duration (seconds)');
-title('inspection of timeframes');
-
-% look at timekeys
-drawnow; figureprep([50 50 800 300],~wanthide); hold on;
+title('inspection of timeframes (dropped frames => NaN => gaps)');
 
 %% Expand simultaneous-keypress cases
 
@@ -86,6 +97,56 @@ for p=1:size(timekeys,1)
     timekeysB(end+1,:) = timekeys(p,:);
   end
 end
+
+%% Deal with some basic timing stuff
+
+% check for dropped frames
+droppedcnt = sum(isnan(timeframes));  % number of dropped frames
+
+% determine last valid recorded timeframe
+ix = find(~isnan(timeframes));
+actualnum = ix(end);
+
+% calculate mean frame-to-frame difference
+  % mdtf = mean(diff(timeframes)); <-- this is the usual version with no NaNs
+mdtf = sum(diff(filterout(timeframes,NaN,0))) / (actualnum-1);
+
+% calc total empirical duration of the experiment (this includes the full completion of the last frame).
+% in the case of partial data (run stopped early), we calculate duration up through the last valid recorded timeframe 
+totaldur = mdtf * actualnum;
+
+% check ending "trigger" time (recorded after last frame)
+ix = find(ismember(timekeysB(:,2),'trigger'));
+assert(length(ix) >= 1);
+if length(ix) < 2
+  warning('*** Did not find the ending "trigger" in timekeys. This must be partial data??? BEWARE! ***');
+  results.numwarnings = results.numwarnings + 1;
+  donetime = NaN;
+else
+  assert(length(ix) == 2);
+  donetime = timekeysB{ix(2),1};
+  timekeysB(ix(2),:) = [];  % NOTE!!! We remove the ending fake trigger to make life easier later
+  if abs(donetime-totaldur) > 0.050
+    warning('**** the donetime and totaldur are mismatched!! BEWARE!!! ***');
+    results.numwarnings = results.numwarnings + 1;
+  end
+end
+
+% record
+results.droppedcnt = droppedcnt;
+results.mdtf = mdtf;
+results.totaldur = totaldur;
+results.donetime = donetime;
+
+%% Interpolate over dropped frames
+
+% use linear interpolation to fill in the NaNs
+ii0 = find(~isnan(timeframes));
+ii1 = find(isnan(timeframes));
+timeframes(ii1) = interp1(ii0,timeframes(ii0),ii1,'linear','extrap');
+
+% record
+results.timeframes = timeframes;  % user must use this version from behavioral analysis and not the version in the raw data!!
 
 %% Deal with terrible button pre-processing stuff
 
@@ -176,7 +237,7 @@ results.buttontimes = buttontimes;
 results.buttonpressed = buttonpressed;
 results.triggertimes = triggertimes;
 
-%% Do some basic counts of buttons and triggers
+%% Do some basic counts of buttons
 
 % count buttons
 userkeycounts = [];
@@ -188,9 +249,51 @@ end
 results.userkeys = userkeys;
 results.userkeycounts = userkeycounts;
 
-%% Do some plotting
+%% Do some checks of triggers
 
-ylim([0 1.5]);
+% calc
+totaln = length(triggertimes);  % total number of triggers detected
+
+% if it seems that this is just a behavior experiment
+if totaln < 5
+
+  warning('*** Detected less than 5 triggers. This is probably a behavior-only experiment? ***');
+  results.numwarnings = results.numwarnings + 1;
+  results.mdf = NaN;
+  results.mdferrs = [];
+  results.numcrazymdferrs = NaN;
+
+% if it seems that this is a run with actual triggers from the scanner
+else
+
+  % calculate median trigger diff
+  mdf = median(diff(triggertimes));  % typical empirical TR diff
+
+  % find diffs that are more than FRAC larger or FRAC smaller than the mdf.
+  % express these as a multiple of the mdf. For example, [1.99860640702692]
+  temp = diff(triggertimes);
+  errfrac = 0.03125;  % .05 s out of 1.6 s
+  mdferrs = temp(temp > mdf*(1+errfrac) | temp < mdf*(1-errfrac)) / mdf;  
+
+  % how many mdf errors are crazy?
+  numcrazymdferrs = sum(abs(mdferrs-round(mdferrs)) > 0.01);
+  if numcrazymdferrs > 0
+    warning('*** We encountered a crazy mdf error; all bets are off!!! ***');
+    results.numwarnings = results.numwarnings + 1;
+  end
+
+  % record
+  results.mdf = mdf;
+  results.mdferrs = mdferrs;
+  results.numcrazymdferrs = numcrazymdferrs;
+
+end
+
+%% Plot the buttons/triggers
+
+% look at timekeys
+drawnow; figureprep([50 50 1200 300],~wanthide); hold on;
+ylim([-.1 1.5]);
 straightline(mristarttime,'v','k-',[0 0.75]);
 cnt = 1;
 for p=1:length(buttontimes)
@@ -198,6 +301,25 @@ for p=1:length(buttontimes)
   text(buttontimes(p),1.1 + 0.3*rand,buttonpressed{p},'HorizontalAlignment','center','Color',getcolorchar(cnt));
   cnt = cnt + 1;
 end
-set(straightline(triggertimes,'v','k-',[0 .5]),'Color',[.7 .7 .7]);
-xlabel('time (seconds)');
-title('inspection of timekeys');
+set(straightline(triggertimes,'v','k-',[-.1 .5]),'Color',[.7 .7 .7]);
+straightline(donetime,'v','k-',[0 0.75]);
+xlabel('Time (seconds)');
+title('Inspection of buttons and triggers');
+xlim0 = xlim;
+xlim([-10 xlim0(2)+10]);
+
+%% Report to the command window
+
+fprintf('==============================================================\n');
+fprintf('Experiment duration (empirical) was %.3f.\n',totaldur);
+fprintf('Frames per sec (empirical) was %.6f.\n',1/mdtf);
+fprintf('Number of dropped frames: %d.\n',droppedcnt);
+fprintf('Median trigger diff: %.6f.\n',mdf);
+fprintf('==============================================================\n');
+fprintf('Number of pressed buttons:\n');
+for pp=1:length(userkeys)
+  fprintf('%s: % 5d\n', ...
+          userkeys{pp}, ...
+          userkeycounts(pp));
+end
+fprintf('==============================================================\n');
